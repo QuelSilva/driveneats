915fd4090ad50e74c6166e75c8fd7bd429941705
user.name <raquellbezerraa20@gmail.com>
2023-01-08 01:08:09 -0400

    adicionei a seleção de sobremesas

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,15 +1,20 @@
+
+/*seleciona os pratos*/
 function selecionarPrato(thisComida){
+    /*busca pela classe no html*/
     const selecionar = document.querySelector(".marcaSecaoComida .selecionado");
 
     console.log(selecionar);
 
     if(selecionar !== null) {
+        /*remove a class*/
         selecionar.classList.remove("selecionado");
     }
-
+    /*adiciona a classe*/
     thisComida.classList.add("selecionado");
 }
 
+/*seleciona as bebidas*/
 function selecionarBebida(thisBebida){
     const selecionar = document.querySelector(".marcaSecaoBebida .selecionado");
 
@@ -20,4 +25,18 @@ function selecionarBebida(thisBebida){
     }
 
     thisBebida.classList.add("selecionado");
+}
+
+
+/*seleciona as sobremesas*/
+function selecionarSobremesa(thisSobremesa){
+    const selecionar = document.querySelector(".marcaSecaoSobremesa .selecionado");
+
+    console.log(selecionar);
+
+    if(selecionar !== null) {
+        selecionar.classList.remove("selecionado");
+    }
+
+    thisSobremesa.classList.add("selecionado");
 }
