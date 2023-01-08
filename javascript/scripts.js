ec49a10c7fa7e995972fa2e27d43a7f960272944
user.name <raquellbezerraa20@gmail.com>
2023-01-08 01:00:10 -0400

    adicionei a seleção dos pratos

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,11 @@
-function selecionarPrato(){
-   alert('testando o JS');
-} 
+function selecionarPrato(thisComida){
+    const selecionar = document.querySelector(".marcaSecaoComida .selecionado");
+
+    console.log(selecionar);
+
+    if(selecionar !== null) {
+        selecionar.classList.remove("selecionado");
+    }
+
+    thisComida.classList.add("selecionado");
+}
